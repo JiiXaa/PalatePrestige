330b11cc09d1d00d4ba9790ff8ebe41cd786ab20
JiiXaa <anythingfx@gmail.com>
2023-05-18 21:47:42 +0100

    Added menus app with Menu and Dish models.

---
+++ menus/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    chef = models.ForeignKey("users.Chef", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.CharField(max_length=100)
+
+    menu = models.ForeignKey(Menu, related_name="dishes", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
