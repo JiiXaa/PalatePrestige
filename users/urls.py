eec11ba040fe52dc405a5b4d1c56a2ad2e87056f
JiiXaa <anythingfx@gmail.com>
2023-05-19 21:02:47 +0100

    CHEF FEATURE ADD: templates for displaying all chefs and theirs detail page with menus offered.

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("chefs/", views.all_chefs, name="chefs")]
+urlpatterns = [
+    path("chefs/", views.all_chefs, name="chefs"),
+    path("chefs/<int:chef_id>/", views.chef_detail, name="chef_detail"),
+]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Chef, Customer
+from menus.models import Menu
 
 
 def all_chefs(request):
@@ -15,8 +16,19 @@ def all_chefs(request):
 
 
 def chef_detail(request, chef_id):
-    """A view to show individual chef details"""
-    return render(request, "users/chef_detail.html")
+    """A view to show individual chef details and their menus"""
+
+    # Get the chef or return 404 if not found
+    chef = get_object_or_404(Chef, id=chef_id)
+    # Get all menus associated with this chef
+    menus = Menu.objects.filter(chef=chef)
+
+    context = {
+        "chef": chef,
+        "menus": menus,
+    }
+
+    return render(request, "chefs/chef_detail.html", context)
 
 
 def all_customers(request):
