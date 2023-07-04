a9a88c713e109662d31b071f463da2262c641d66
JiiXaa <anythingfx@gmail.com>
2023-07-04 17:50:37 +0100

    Added custom css styles to the edit chef profile. Added custom filter to be able show textarea element in the form.

--- users/templatetags/custom_filters.py
+++ users/templatetags/custom_filters.py
@@ -7,3 +7,10 @@ register = template.Library()
 @register.filter
 def split_string(value, separator):
     return value.split(separator)
+
+
+# Custom filter to get the class name of an object used in the chef profile page to show the  textareas elements
+# solution found here: https://stackoverflow.com/questions/4938303/how-to-properly-make-custom-filter-in-django-framework
+@register.filter(name="class_name")
+def class_name(value):
+    return value.__class__.__name__
